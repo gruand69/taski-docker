8138371cd107da10703931fe4a27300f9e60b394
gruand69 <gruand69@yandex.ru>
2025-11-06 20:10:41 +0300

    check with kittygram

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Task(models.Model):
+    """Task model"""
     title = models.CharField(verbose_name='Заголовок', max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
